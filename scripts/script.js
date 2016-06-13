b0690eb11bbcc6e4ea4eb25ca292ae1461f6e767
eux <ixue@163.com>
2016-06-13 17:20:47 +0800

    Try add loop turn.

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,10 @@
 $(function() {
+
 	render(40);
 
-	const grid = $(".grid");
-	$(document).keydown(function(event) {
-		switch (event.which) {
-			case 37:
-			event.preventDefault();
-			snake.direction = "Left";
-			console.log(snake.direction);
-			break;
-			case 38:
-			event.preventDefault();
-			snake.direction = "Up";
-			console.log(snake.direction);
-			break;
-			case 39:
-			event.preventDefault();
-			snake.direction = "Right";
-			console.log(snake.direction);
-			break;
-			case 40:
-			event.preventDefault();
-			snake.direction = "Down";
-			console.log(snake.direction);
-			break;
-			default:
-			console.log("Excuse me? " + event.which);
-		}
-	});
+	$(document).keydown(keys);
+
+	setTimeout(move, 100);
 });
 
 function render(gridNum) {
@@ -43,6 +20,33 @@ function render(gridNum) {
 	}
 }
 
+function keys(event) {
+	switch (event.which) {
+		case 37:
+		event.preventDefault();
+		snake.direction = "Left";
+		console.log(snake.direction);
+		break;
+		case 38:
+		event.preventDefault();
+		snake.direction = "Up";
+		console.log(snake.direction);
+		break;
+		case 39:
+		event.preventDefault();
+		snake.direction = "Right";
+		console.log(snake.direction);
+		break;
+		case 40:
+		event.preventDefault();
+		snake.direction = "Down";
+		console.log(snake.direction);
+		break;
+		default:
+		console.log("Excuse me? " + event.which);
+	}
+}
+
 let snake = {
 	position: [20, 20],
 	direction: "r"
@@ -50,6 +54,6 @@ let snake = {
 
 let currentPos = [[20, 20]];
 
-function setTimeout(time) {
-
+function move() {
+	console.log("New turn");
 }
