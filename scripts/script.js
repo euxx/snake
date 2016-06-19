33814b9b35b6194f185203392038cd36ea426ea5
eux <ixue@163.com>
2016-06-19 22:15:21 +0800

    Make snake add length by eatting food.

--- scripts/script.js
+++ scripts/script.js
@@ -36,41 +36,16 @@ function gamePause(event) {
 function directionControl(event) {
 	if ([37, 38, 39, 40].indexOf(event.which) > -1) {
 		event.preventDefault();
-		let keyWhich;
-		switch (event.which) {
-			case 37:
-			keyWhich = "l";
-			break;
-			case 38:
-			keyWhich = "u";
-			break;
-			case 39:
-			keyWhich = "r";
-			break;
-			case 40:
-			keyWhich = "d";
-			break;
-			default:
-			break;
-		}
 
-		if (currentDir != keyWhich ) {
-			switch (event.which) {
-				case 37:
-				currentDir = "l";
-				break;
-				case 38:
-				currentDir = "u";
-				break;
-				case 39:
-				currentDir = "r";
-				break;
-				case 40:
-				currentDir = "d";
-				break;
-				default:
-				break;
-			}
+		const key = event.which;
+		if (key === 37 && currentDir != "l") {
+			currentDir = "l";
+		} else if (key === 38 && currentDir != "u") {
+			currentDir = "u";
+		} else if (key === 39 && currentDir != "r") {
+			currentDir = "r";
+		} else if (key === 40 && currentDir != "d") {
+			currentDir = "d";
 		}
 		console.log(currentDir);
 
@@ -80,7 +55,7 @@ function directionControl(event) {
 }
 
 let snake = {
-	position: [[4, 20], [4, 18]],
+	position: [[4, 20], [4,19], [4, 18]],
 	direction: "r"
 };
 let currentPos = snake.position;
@@ -94,18 +69,42 @@ let foodPos = food.position;
 let cpx = currentPos[0][0];
 let cpy = currentPos[0][1];
 
+function foodxy() {
+  const xy = [Math.floor(Math.random()*38) + 2, Math.floor(Math.random()*38) + 2]
+	console.log("food  " + xy);
+	return xy;
+}
+
+function foodDisplay(posArray) {
+		$(".grid").removeClass("food");
+		$(".row").eq(foodPos[0]).find(".grid").eq(foodPos[1]).addClass("food");
+}
+
+function snakeDisplay(posArray) {
+	$(".grid").removeClass("snake-body");
+	let i = 0;
+	for ( ; i < posArray.length; i++) {
+		const x = posArray[i][0];
+		const y = posArray[i][1];
+		$(".row").eq(x).find(".grid").eq(y).addClass("snake-body");
+	//  $(".row:nth-child(" + x + ") .grid:nth-child(" + y + ")").addClass("test");
+	}
+}
+
 function move() {
-	if (cpy < 1 || cpy > 38 || cpx < 1 ||cpx > 38) {
+	if (cpy < 1 || cpy > 39 || cpx < 1 ||cpx > 39) {
 		console.log("Game over ):");
 		return;
 	}
 
-	let i = 1;
-	for ( ; i < currentPos.length; i++) {
-		let x = currentPos[i - 1];
-		currentPos[i] = x;
+	let lastPos = currentPos[currentPos.length - 1];
+	lastPos = [lastPos[0], lastPos[1]];
+
+	let i = currentPos.length;
+	for ( ; i > 1; i--) {
+		currentPos[i - 1][0] = currentPos[i - 2][0];
+		currentPos[i - 1][1] = currentPos[i - 2][1];
 	}
-	console.log(currentPos[0] + "  " + currentPos[1]);
 
 	switch (currentDir) {
 		case "l":
@@ -126,36 +125,14 @@ function move() {
 
 	currentPos[0][0] = cpx;
 	currentPos[0][1] = cpy;
-	console.log(currentPos[0] + "  " + currentPos[1]);
 	if (cpx == foodPos[0] && cpy == foodPos[1]) {
 		console.log("Got you");
 		foodPos = foodxy();
 		foodDisplay(foodPos);
-		currentPos.push(currentPos[0]);
+		currentPos.push(lastPos);
 	}
 
 	snakeDisplay(currentPos);
-	$(document).keydown(directionControl);
-	setTimeout(move, 400);
-	console.log("New turn");
-}
-
-function foodxy() {
-	return  [Math.floor(Math.random()*40 + 1), Math.floor(Math.random()*40 + 1)];
-}
-
-function foodDisplay(posArray) {
-		$(".grid").removeClass("food");
-		$(".row").eq(foodPos[0]).find(".grid").eq(foodPos[1]).addClass("food");
-}
-
-function snakeDisplay(posArray) {
-	$(".grid").removeClass("snake-body");
-	let i = 0;
-	for ( ; i < posArray.length; i++) {
-		const x = posArray[i][0];
-		const y = posArray[i][1];
-		$(".row").eq(x).find(".grid").eq(y).addClass("snake-body");
-	//  $(".row:nth-child(" + x + ") .grid:nth-child(" + y + ")").addClass("test");
-	}
+	$(window).keydown(directionControl);
+	setTimeout(move, 200);
 }
